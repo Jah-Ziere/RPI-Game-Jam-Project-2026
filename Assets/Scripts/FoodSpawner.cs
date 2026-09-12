ecd315c02b218b59bb59aa9e4e7912b9ba4348b9
Jah-ziere <jahzierejohnsonm@gmail.com>
2026-09-12 19:22:49 -0400

    Food spawn mechanic
    Created a food spawner which spawns food every few seconds at a certain range'

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    public float spawnInterval = 1.5f;
+    public float spawnRangeX = 10f;
+    public float spawnRangeY = 6f;
+    private float timer = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+    if (timer >= spawnInterval)
+    {
+        timer = 0f;
+        SpawnFood();
+    }
+
+    }
+
+    void SpawnFood()
+    {
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        float randomY = Random.Range(-spawnRangeY, spawnRangeY);
+
+        Vector3 spawnPosition = transform.position + new Vector3(randomX, randomY, 0f);
+
+        Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
+
+    }   
+}
