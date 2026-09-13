51584d3b8433bdd73f1b3166f7f6307cbfd2dbec
Jah-ziere <jahzierejohnsonm@gmail.com>
2026-09-12 22:34:27 -0400

    Change Forms
    Now at a certain level you will change forms

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,9 +11,18 @@ public class Player : MonoBehaviour
     public int foodToNextLevel = 5;
     public int level = 1;
 
+    public Sprite fishSprite;
+    public Sprite sharkSprite;
+    public Sprite megSprite;
+    private SpriteRenderer spriteRenderer;
+    private int stage = 0;  
+
     void Start()
 {
     rb = GetComponent<Rigidbody2D>();
+
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    spriteRenderer.sprite = fishSprite;
 }
 
 void Update()
@@ -46,7 +55,23 @@ void LevelUp()
 {
     level++;
     foodEaten = 0;
+    foodToNextLevel = 5 + (level - 1);
     transform.localScale += new Vector3(0.2f, 0.2f, 0f);
+     CheckStage();
+}
+
+void CheckStage()
+{
+    if (level >= 50 && stage < 2)
+    {
+        stage = 2;
+        spriteRenderer.sprite = megSprite;
+    }
+    else if (level >= 20 && stage < 1)
+    {
+        stage = 1;
+        spriteRenderer.sprite = sharkSprite;
+    }
 }
     
 }
